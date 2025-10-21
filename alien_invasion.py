dc60dd30a2674e9f2c21d6a232ce49c1172c9b60
stuart-wagner <ww289546@icloud.com>
2025-10-20 22:06:54 -0500

    increasing difficulty (287)

--- alien_invasion.py
+++ alien_invasion.py
@@ -131,6 +131,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _update_aliens(self):
         """
@@ -223,6 +224,9 @@ class AlienInvasion:
         """Start a new game when the player clicks play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            #reset settings
+            self.settings.initialize_dynamic_settings()
+
             # reset game stats
             self.stats.reset_stats()
             self.stats.game_active = True

28c92b2f58d08f4b37b24266c72f77d726a9db91
stuart-wagner <ww289546@icloud.com>
2025-10-20 22:41:47 -0500

    GAME COMPLETE LETS GOOOOOO

--- alien_invasion.py
+++ alien_invasion.py
@@ -210,8 +210,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            #Decrement ships left
+            #Decrement ships left, update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of remaining aliens
             self.aliens.empty()
@@ -248,6 +249,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # get rid of any aliens and bullets
             self.aliens.empty()
